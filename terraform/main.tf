836fef20b509c53343ac5eb24f7d443a8e76fa5f
RCdeWit <RCdeWit@users.noreply.github.com>
2025-11-26 22:32:26 +0100

    Use local backend for now

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,6 @@
 terraform {
   required_version = "1.9.5"
 
-  backend "s3" {
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
-    use_path_style              = true
-  }
-
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
@@ -34,9 +25,4 @@ provider "hcloud" {
 
 provider "hetznerdns" {
   api_token = var.hetznerdns_token
-}
-
-provider "tailscale" {
-  api_key = var.tailscale_api_key
-  tailnet = var.tailnet
 }
