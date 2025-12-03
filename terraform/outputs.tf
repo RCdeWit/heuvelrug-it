60b12141b3cf93e6981aaab9d2489350c065bd99
RCdeWit <RCdeWit@users.noreply.github.com>
2025-12-03 17:26:19 +0100

    Output provisioned volume

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,7 @@ output "vps_ip" {
   description = "IPv4 for the VPS"
   value       = hcloud_server.drive_instance.ipv4_address
 }
+
+output "volume_device" {
+  value = "/dev/disk/by-id/scsi-0HC_Volume_${hcloud_volume.volume1.id}"
+}
